dc9da63f234e6ce66b64a647b467b7a064c284cc
Maria Eduarda Guimarães Carvalho <146151781+Madu-Guimaraes@users.noreply.github.com>
2024-12-27 08:37:52 -0400

    add extras

--- lib/screens/vehicle_screen.dart
+++ lib/screens/vehicle_screen.dart
@@ -89,159 +89,153 @@ class _VehicleScreenState extends State<VehicleScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          SingleChildScrollView(
-            child: Column(
-              children: [
-                _buildCarImage(), // Imagem do carro
-                const SizedBox(height: 20),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  child: Form(
-                    key: _formKey,
-                    child: Column(
-                      children: [
-                        // Inputs fixos
-                        InputField(
-                          label: "Digite o Modelo do Automóvel",
-                          icon: Icons.drive_eta,
-                          borderBottom: false,
-                          controller: _modelController,
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Este campo é obrigatório';
-                            }
-                            return null;
-                          },
-                        ),
-                        const SizedBox(height: 15),
-                        InputField(
-                          label: "Digite a Marca do Automóvel",
-                          icon: Icons.branding_watermark,
-                          borderBottom: false,
-                          controller: _brandController,
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Este campo é obrigatório';
-                            }
-                            return null;
-                          },
-                        ),
-                        const SizedBox(height: 15),
-                        InputField(
-                          label: "Digite o Ano do Automóvel",
-                          icon: Icons.calendar_today,
-                          borderBottom: false,
-                          keyboardType: TextInputType.number,
-                          controller: _yearController,
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Este campo é obrigatório';
-                            }
-                            if (!RegExp(r'^\d{4}$').hasMatch(value)) {
-                              return 'O ano deve ser no formato AAAA';
-                            }
-                            return null;
-                          },
-                        ),
-                        const SizedBox(height: 20),
-
-                        // Inputs adicionais
-                        ...additionalInputs.map((input) {
-                          return Column(
-                            children: [
-                              InputField(
-                                label: "Digite o Modelo do Automóvel",
-                                icon: Icons.drive_eta,
-                                borderBottom: false,
-                                controller: input['modelController'],
-                                validator: (value) {
-                                  if (value == null || value.isEmpty) {
-                                    return 'Este campo é obrigatório';
-                                  }
-                                  return null;
-                                },
-                              ),
-                              const SizedBox(height: 15),
-                              InputField(
-                                label: "Digite a Marca do Automóvel",
-                                icon: Icons.branding_watermark,
-                                borderBottom: false,
-                                controller: input['brandController'],
-                                validator: (value) {
-                                  if (value == null || value.isEmpty) {
-                                    return 'Este campo é obrigatório';
-                                  }
-                                  return null;
-                                },
-                              ),
-                              const SizedBox(height: 15),
-                              InputField(
-                                label: "Digite o Ano do Automóvel",
-                                icon: Icons.calendar_today,
-                                borderBottom: false,
-                                keyboardType: TextInputType.number,
-                                controller: input['yearController'],
-                                validator: (value) {
-                                  if (value == null || value.isEmpty) {
-                                    return 'Este campo é obrigatório';
-                                  }
-                                  if (!RegExp(r'^\d{4}$').hasMatch(value)) {
-                                    return 'O ano deve ser no formato AAAA';
-                                  }
-                                  return null;
-                                },
-                              ),
-                              const SizedBox(height: 20),
-                            ],
-                          );
-                        }).toList(),
-
-                        // Botão para adicionar mais veículos
-                        TextButton(
-                          onPressed: _addVehicleInput,
-                          child: Row(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              const Icon(Icons.add),
-                              const SizedBox(width: 8),
-                              const Text('Adicionar mais veículos'),
-                            ],
-                          ),
-                        ),
-
-                        // Botão "X" para fechar todos os inputs adicionados
-                        if (additionalInputs.isNotEmpty)
-                          Positioned(
-                            right: 20,
-                            top: 100,
-                            child: FloatingActionButton(
-                              mini: true,
-                              onPressed: _clearAllAdditionalInputs,
-                              child: const Icon(Icons.close),
-                            ),
-                          ),
-
-                        // Botão para submeter
-                        SizedBox(
-                          height: 40,
-                          width: 200,
-                          child: CustomButton(
-                            label: 'Submeter',
-                            color: const Color(0xFF2236BD),
-                            textColor: Colors.white,
-                            onPressed: _submitData,
-                          ),
-                        ),
-                      ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            _buildCarImage(), // Imagem do carro
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    // Inputs fixos
+                    InputField(
+                      label: "Digite o Modelo do Automóvel",
+                      icon: Icons.drive_eta,
+                      borderBottom: false,
+                      controller: _modelController,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Este campo é obrigatório';
+                        }
+                        return null;
+                      },
                     ),
-                  ),
+                    const SizedBox(height: 15),
+                    InputField(
+                      label: "Digite a Marca do Automóvel",
+                      icon: Icons.branding_watermark,
+                      borderBottom: false,
+                      controller: _brandController,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Este campo é obrigatório';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 15),
+                    InputField(
+                      label: "Digite o Ano do Automóvel",
+                      icon: Icons.calendar_today,
+                      borderBottom: false,
+                      keyboardType: TextInputType.number,
+                      controller: _yearController,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Este campo é obrigatório';
+                        }
+                        if (!RegExp(r'^\d{4}$').hasMatch(value)) {
+                          return 'O ano deve ser no formato AAAA';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 20),
+
+                    // Inputs adicionais
+                    ...additionalInputs.map((input) {
+                      return Column(
+                        children: [
+                          InputField(
+                            label: "Digite o Modelo do Automóvel",
+                            icon: Icons.drive_eta,
+                            borderBottom: false,
+                            controller: input['modelController'],
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Este campo é obrigatório';
+                              }
+                              return null;
+                            },
+                          ),
+                          const SizedBox(height: 15),
+                          InputField(
+                            label: "Digite a Marca do Automóvel",
+                            icon: Icons.branding_watermark,
+                            borderBottom: false,
+                            controller: input['brandController'],
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Este campo é obrigatório';
+                              }
+                              return null;
+                            },
+                          ),
+                          const SizedBox(height: 15),
+                          InputField(
+                            label: "Digite o Ano do Automóvel",
+                            icon: Icons.calendar_today,
+                            borderBottom: false,
+                            keyboardType: TextInputType.number,
+                            controller: input['yearController'],
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Este campo é obrigatório';
+                              }
+                              if (!RegExp(r'^\d{4}$').hasMatch(value)) {
+                                return 'O ano deve ser no formato AAAA';
+                              }
+                              return null;
+                            },
+                          ),
+                          const SizedBox(height: 20),
+                        ],
+                      );
+                    }),
+
+                    // Botão para adicionar mais veículos
+                    TextButton(
+                      onPressed: _addVehicleInput,
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          const Icon(Icons.add),
+                          const SizedBox(width: 8),
+                          const Text('Adicionar mais veículos'),
+                        ],
+                      ),
+                    ),
+
+                    // Botão "X" para limpar todos os inputs adicionais
+                    if (additionalInputs.isNotEmpty)
+                      Center(
+                        child: FloatingActionButton(
+                          mini: true,
+                          onPressed: _clearAllAdditionalInputs,
+                          child: const Icon(Icons.close),
+                        ),
+                      ),
+
+                    // Botão para submeter
+                    SizedBox(
+                      height: 40,
+                      width: 200,
+                      child: CustomButton(
+                        label: 'Submeter',
+                        color: const Color(0xFF2236BD),
+                        textColor: Colors.white,
+                        onPressed: _submitData,
+                      ),
+                    ),
+                  ],
                 ),
-              ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
